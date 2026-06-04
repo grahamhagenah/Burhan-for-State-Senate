8e28d6dcc5ece8ac27c2d2da062fe632ac2a3c2d
Niki Aggarwal <sayhellotoniki@gmail.com>
2026-06-04 18:51:14 -0400

    instagram embed width

--- project/site/issues.jsx
+++ project/site/issues.jsx
@@ -98,7 +98,7 @@ function InstagramEmbed({ url }) {
         className="instagram-media"
         data-instgrm-permalink={`${url}?utm_source=ig_embed&utm_campaign=loading`}
         data-instgrm-version="14"
-        style={{ background: "#FFF", border: 0, borderRadius: 3, margin: "0 auto", maxWidth: 540, minWidth: 0, padding: 0, width: "100%" }}
+        style={{ background: "#FFF", border: 0, borderRadius: 3, margin: "0 auto", maxWidth: "100%", minWidth: 0, padding: 0, width: "100%" }}
       />
     </div>
   );
